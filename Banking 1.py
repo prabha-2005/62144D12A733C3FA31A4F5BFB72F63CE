3fbcec28f7a378b894bd70e41fcf0ac45b4a3bba
prabha-2005 <144514435+prabha-2005@users.noreply.github.com>
2023-09-22 18:52:19 +0530

    Add files via upload

---
+++ Banking 1.py
@@ -0,0 +1,41 @@
+class bankAccount :_
+
+  
+
+ def  __ int__ (self , account_number , account_holder_name, initial_blance=0.0):
+
+  self.account_number= account_number
+
+  self.__account_holder_name= account_holder_name
+ 
+ self.__account_balance=initial_balance
+
+ 
+  def deposit(self, amount):
+    if amount > 0:
+
+    self.__account_balance += amount
+ 
+   print( "Deposited${}. NEW balance: ${}". format(amount,self.__account_balance))
+
+else:
+
+ print("Invaleid deposeit amount.please depose a positive amount.")
+
+
+  def withdraw(self, amount):
+
+ if amount > 0 and amount <=self.__account_balance:
+
+      self.__account_balance _= amount
+ 
+    print("withdrew ${}. New balance: ${}".format(amount,self.__account_balance))
+
+else:
+
+ print("Invalid withdrawal amount or insufficient balance.")
+
+
+  def display_balance(self):
+ 
+    print("Account balance for {}")
---
+++ cricket.py
@@ -0,0 +1,38 @@
+# Define the base class player
+
+class player:
+   def play(self):
+
+    print("the player is playing cricket.")
+
+
+# Define the derived class Batsman
+
+class Batasman(player):
+ 
+   def player(self):
+
+    print('the batsman is batsman.')
+
+
+
+# Define the derived class Bowler
+
+class Bowler(player):
+  
+ defplay(self):
+  
+ print("the bowler is bowling.")
+
+
+# Create objects of Batsman and Bowler classes
+
+batsman = Batsman()
+
+bowler = Bowler()
+
+# Call  the play() method for each other
+
+batsman.play()
+
+bowler.play()
